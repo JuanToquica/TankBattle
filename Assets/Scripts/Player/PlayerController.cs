c11836723486ec4666b71e4835a4357b23f30287
JuanToquica <juanestebantoquicagonzalez@gmail.com>
2025-03-13 00:14:43 -0500

    Importacion asset tanque

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,3 @@
+using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+using UnityEngine.InputSystem;
