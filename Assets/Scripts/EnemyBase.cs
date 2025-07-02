ba7026d3b9aff98113d808f8f428a82d304d8323
JuanToquica <juanestebantoquicagonzalez@gmail.com>
2025-07-02 02:16:39 -0500

    Vfx de bandera creado

--- Assets/Scripts/EnemyBase.cs
+++ Assets/Scripts/EnemyBase.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
+    [SerializeField] private GameObject vfx;
     private void OnTriggerStay(Collider other)
     {
         if (other.transform.CompareTag("Player") && GameManager.instance.playerHasTheFlag)
         {
             GameManager.instance.OnFlagDelivered();
+            Instantiate(vfx, transform.position + transform.up, transform.rotation);
         }
     }
 }
